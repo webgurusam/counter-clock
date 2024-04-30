4789865a943bfd5cfe52197ddc9f44c1d84da47b
webgurusam <shyamghosh666@gmail.com>
2024-04-30 14:16:29 +0600

    added reset & everything in counter clock

--- js/counter-clock.js
+++ js/counter-clock.js
@@ -4,20 +4,23 @@ let sec = 0;
 let min = 0;
 let hr = 0;
 
-function start(){
-    if(isStop == true){
+// start counter clock by this function
+const start = () => {
+    if(!!isStop){
         isStop = false;
         timer();
     }
 }
 
-function timer(){
-    if(isStop == false){
+// counting up by timer function
+const timer = () => {
+    if(!isStop){
 
         sec = parseInt(sec);
         min = parseInt(min);
         hr = parseInt(hr);
 
+        // increasing the timer value
         sec++;
         
         if(sec == 60){
@@ -32,28 +35,45 @@ function timer(){
             hr = 0;
         }
 
+        // set zero before counting up time
         if(sec < 10){
-            sec = '0' + sec;
+            second.innerHTML = '0' + sec;
         }
         if(min < 10){
-            min = '0' + min;
+            min.innerHTML = '0' + min;
         }
         if(hr < 10){
-            hr = '0' + hr;
+            hr.innerHTML = '0' + hr;
         }
 
-        second.innerHTML = sec;
-        min.innerHTML = min;
-        hr.innerHTML = hr;
-
-        setTimeout('timer()', 1000);
+        // calling timer function after 1 second
+        setTimeout(timer, 1000);
     }
 }
 
-function stop(){
+// stop the timer by stop function
+const stop = () => {
     isStop = true;
 }
 
-function reset(){
-    
+// reset all counting up values by reset function
+const reset = () => {
+    isStop = true;
+
+    if(sec < 60){
+        sec = 0;
+        sec = '0' + sec;
+    }
+    if(min < 60){
+        min = 0;
+        min = '0' + min;
+    }
+    if(hr < 24){
+        hr = 0;
+        hr = '0' + hr;
+    }
+
+    second.innerHTML = sec;
+    min.innerHTML = min;
+    hr.innerHTML = hr;
 }
